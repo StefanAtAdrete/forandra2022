ee05d9cf554fc47cd7636546187830cfb0378112
Stefan Carlsson <stefan@adrete.se>
2022-08-10 09:54:45 +0200

    Tracking code for mautic

--- public_html/themes/custom/forandra_subtheme/js/global.js
+++ public_html/themes/custom/forandra_subtheme/js/global.js
@@ -10,6 +10,13 @@
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
 
+    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://crm.forandra.nu/docroot/mtc.js','mt');
+
+    mt('send', 'pageview');
+
     }
   };
 
